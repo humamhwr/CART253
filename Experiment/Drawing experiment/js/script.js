e52c4c95dc54778f2247aada8fc59c8bc571e647
Humam Hawara <himoo1999@gmail.com>
2020-09-10 17:16:41 -0400

    started drawing exp and creating files

---
+++ Experiment/Drawing experiment/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(255,0,0);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
