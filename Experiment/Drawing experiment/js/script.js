84fa6354236ff4ce4ade361cf24087f68473518f
Humam Hawara <himoo1999@gmail.com>
2020-09-11 13:53:00 -0400

    02 - Drawing shapes

--- Experiment/Drawing experiment/js/script.js
+++ Experiment/Drawing experiment/js/script.js
@@ -11,13 +11,17 @@ Here is a description of this template p5 project.
 function setup() {
 createCanvas(500,500);
 
-  background(200,200,200);
-  ellipse(250,250,100,100);
+  background(255,138,1);
 
-  rect(0,0,250,250);
-  line(0,0,500,500);
-  line(500,0,0,500);
-  point(250,250);
+  ellipseMode(CORNER);
+  ellipse(250,250,100,100);
+  ellipse(250,250,80,80);
+  ellipse(250,250,60,60);
+  ellipse(250,250,40,40);
+  ellipse(250,250,20,20);
+
+
+
 
 
 }
