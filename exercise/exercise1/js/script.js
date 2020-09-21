d5874ac27119f9a083a6abaf91e9f94985e20895
Humam Hawara <himoo1999@gmail.com>
2020-09-21 19:50:49 -0400

    E1: added 2 yellow circles on the side, and one big transparent circle

--- exercise/exercise1/js/script.js
+++ exercise/exercise1/js/script.js
@@ -4,20 +4,37 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-let backgroundShade = 0;
-
-let circle = {
-  x:250,
-  y:500,
-  size:100,
-  speed:9,
-  fill:255
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+};
+let sun = {
+  x: 250,
+  y: 0,
+  size: 100,
+  speed: 1,
+};
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  growthrate: 1,
+  speed: 1,
+  fill: 255,
+  alpha:200
+};
+let sun2 = {
+  x: 250,
+  y: 400,
+  size: 100,
+  speed: 1,
 };
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 
 }
 
@@ -25,14 +42,31 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(backgroundShade);
+  background(255,69,0);
+bg.r = map(sun.size,100,width,0,255);
 
-  circle.x = circle.x + circle.speed;
-  // circle.x = constrain(circle.x,0,width);
 
-circle.size = map(circle.x,0,width,0,255);
-  fill(circle.fill);
-  rectMode(CENTER)
-  rect(circle.x, circle.y, circle.size);
+//drawing the sun and it's rising
+  sun.x = sun.x + sun.speed;
+  sun.x = constrain(sun.x,0,width);
+  sun.size = map(sun.x, 0, width, 100, 300);
+  noStroke();
+  fill(255,255,0);
+  ellipse(sun.x, sun.y, sun.size);
+
+  //drawing a circle
+  circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x,0,width/2);
+  circle1.size = circle1.size + circle1.growthrate;
+  circle1.size = constrain(circle1.size,0,width,500);
+  fill(circle1.fill,circle1.alpha);
+  ellipse(circle1.x,circle1.y,circle1.size);
+
+  sun2.x = sun2.x + sun2.speed;
+  sun2.x = constrain(sun2.x,0,width);
+  sun2.size = map(sun2.x, 0, width, 100, 300);
+  noStroke();
+  fill(255,255,0);
+  ellipse(sun2.x, sun2.y, sun2.size);
 
 }
