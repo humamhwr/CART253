027091561410b3efa281d4396b647cf141a2b717
Humam Hawara <himoo1999@gmail.com>
2020-09-21 20:37:53 -0400

    E1: submission push
    Commented and beautified the code

--- exercise/exercise1/js/script.js
+++ exercise/exercise1/js/script.js
@@ -1,14 +1,18 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Excersie #1
+Hummam Houara
 
-Here is a description of this template p5 project.
+Drew 5 circles that 4 of them cross sides and change sizes;
+ the 5th circle is transparent and it gets bigger in the middle lastly there
+ is a square in the middle that can change sizes according to the mouse's movment.
+ The background changes colour from black to green.
 **************************************************/
 let bg = {
   r: 0,
   g: 0,
   b: 0
 };
+
 let upperLcircle = {
   x: 0,
   y: 0,
@@ -35,13 +39,18 @@ let bottomRcircle = {
 };
 
 let midCircle = {
-  x: 0,
+  x: 250,
   y: 250,
   size: 100,
   growthrate: 1,
   speed: 1,
   fill: 255,
   alpha:200
+};
+let square = {
+x: 250,
+y: 250,
+size:100
 };
 
 // setup()
@@ -60,7 +69,7 @@ function draw() {
 bg.g = map(upperLcircle.size,100,width,20,238,27);
 
 
-//drawing the upperLcircle and it's rising
+//drawing the Upper Left Circle
   upperLcircle.x = upperLcircle.x + upperLcircle.speed;
   upperLcircle.x = constrain(upperLcircle.x,0,width);
   upperLcircle.size = map(upperLcircle.x, 0, width, 100, 300);
@@ -68,7 +77,7 @@ bg.g = map(upperLcircle.size,100,width,20,238,27);
   fill(255,255,0);
   ellipse(upperLcircle.x, upperLcircle.y, upperLcircle.size);
 
-//  bottomLcircle
+ //drawing the Bottom Left Circle
   bottomLcircle.x = bottomLcircle.x + bottomLcircle.speed;
   bottomLcircle.x = constrain(bottomLcircle.x,0,width);
   bottomLcircle.size = map(bottomLcircle.x, 0, width, 100, 300);
@@ -76,7 +85,7 @@ bg.g = map(upperLcircle.size,100,width,20,238,27);
   fill(255,255,0);
   ellipse(bottomLcircle.x, bottomLcircle.y, bottomLcircle.size);
 
-  //  bottomRcircle
+  // //drawing the Upper Right Circle
     upperRcircle.x = upperRcircle.x + upperRcircle.speed;
     upperRcircle.x = constrain(upperRcircle.x,0,width);
     upperRcircle.size = map(upperRcircle.x, 0, width, 100, 300);
@@ -84,7 +93,7 @@ bg.g = map(upperLcircle.size,100,width,20,238,27);
     fill(255,0,0);
     ellipse(upperRcircle.x, upperRcircle.y, upperRcircle.size);
 
-    //  bottomRcircle
+    //drawing the Bottom Right Circle
       bottomRcircle.x = bottomRcircle.x + bottomRcircle.speed;
       bottomRcircle.x = constrain(bottomRcircle.x,0,width);
       bottomRcircle.size = map(bottomRcircle.x, 0, width, 100, 300);
@@ -93,7 +102,7 @@ bg.g = map(upperLcircle.size,100,width,20,238,27);
       ellipse(bottomRcircle.x, bottomRcircle.y, bottomRcircle.size);
 
 
-  //drawing a circle
+  //drawing the middle circle
   midCircle.x = midCircle.x + midCircle.speed;
   midCircle.x = constrain(midCircle.x,0,width/2);
   midCircle.size = midCircle.size + midCircle.growthrate;
@@ -101,7 +110,8 @@ bg.g = map(upperLcircle.size,100,width,20,238,27);
   fill(midCircle.fill,midCircle.alpha);
   ellipse(midCircle.x,midCircle.y,midCircle.size);
 
-  rect(250,250,mouseX,mouseY);
-
-
+//drawing the square
+  fill(0,0,255);
+  rectMode(CENTER);
+  rect(square.x,square.y,mouseX,mouseY);
 }
