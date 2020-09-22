0a2eb946803f9e9ad85dd6eebc7cc7de28783136
Humam Hawara <himoo1999@gmail.com>
2020-09-21 20:11:20 -0400

    E1: added colours and two more circles to cross each other

--- exercise/exercise1/js/script.js
+++ exercise/exercise1/js/script.js
@@ -9,13 +9,32 @@ let bg = {
   g: 0,
   b: 0
 };
-let sun = {
-  x: 250,
+let upperLcircle = {
+  x: 0,
   y: 0,
   size: 100,
   speed: 1,
 };
-let circle1 = {
+let bottomLcircle = {
+  x: 0,
+  y: 500,
+  size: 100,
+  speed: 1,
+};
+let upperRcircle = {
+  x: 500,
+  y: 0,
+  size: 100,
+  speed: -1,
+};
+let bottomRcircle = {
+  x: 500,
+  y: 500,
+  size: 100,
+  speed: -1,
+};
+
+let midCircle = {
   x: 0,
   y: 250,
   size: 100,
@@ -24,12 +43,7 @@ let circle1 = {
   fill: 255,
   alpha:200
 };
-let sun2 = {
-  x: 250,
-  y: 400,
-  size: 100,
-  speed: 1,
-};
+
 // setup()
 //
 // Description of setup() goes here.
@@ -42,31 +56,52 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(255,69,0);
-bg.r = map(sun.size,100,width,0,255);
+  background(bg.r, bg.g, bg.b);
+bg.g = map(upperLcircle.size,100,width,20,238,27);
 
 
-//drawing the sun and it's rising
-  sun.x = sun.x + sun.speed;
-  sun.x = constrain(sun.x,0,width);
-  sun.size = map(sun.x, 0, width, 100, 300);
+//drawing the upperLcircle and it's rising
+  upperLcircle.x = upperLcircle.x + upperLcircle.speed;
+  upperLcircle.x = constrain(upperLcircle.x,0,width);
+  upperLcircle.size = map(upperLcircle.x, 0, width, 100, 300);
   noStroke();
   fill(255,255,0);
-  ellipse(sun.x, sun.y, sun.size);
+  ellipse(upperLcircle.x, upperLcircle.y, upperLcircle.size);
+
+//  bottomLcircle
+  bottomLcircle.x = bottomLcircle.x + bottomLcircle.speed;
+  bottomLcircle.x = constrain(bottomLcircle.x,0,width);
+  bottomLcircle.size = map(bottomLcircle.x, 0, width, 100, 300);
+  noStroke();
+  fill(255,255,0);
+  ellipse(bottomLcircle.x, bottomLcircle.y, bottomLcircle.size);
+
+  //  bottomRcircle
+    upperRcircle.x = upperRcircle.x + upperRcircle.speed;
+    upperRcircle.x = constrain(upperRcircle.x,0,width);
+    upperRcircle.size = map(upperRcircle.x, 0, width, 100, 300);
+    noStroke();
+    fill(255,0,0);
+    ellipse(upperRcircle.x, upperRcircle.y, upperRcircle.size);
+
+    //  bottomRcircle
+      bottomRcircle.x = bottomRcircle.x + bottomRcircle.speed;
+      bottomRcircle.x = constrain(bottomRcircle.x,0,width);
+      bottomRcircle.size = map(bottomRcircle.x, 0, width, 100, 300);
+      noStroke();
+      fill(255,0,0);
+      ellipse(bottomRcircle.x, bottomRcircle.y, bottomRcircle.size);
+
 
   //drawing a circle
-  circle1.x = circle1.x + circle1.speed;
-  circle1.x = constrain(circle1.x,0,width/2);
-  circle1.size = circle1.size + circle1.growthrate;
-  circle1.size = constrain(circle1.size,0,width,500);
-  fill(circle1.fill,circle1.alpha);
-  ellipse(circle1.x,circle1.y,circle1.size);
+  midCircle.x = midCircle.x + midCircle.speed;
+  midCircle.x = constrain(midCircle.x,0,width/2);
+  midCircle.size = midCircle.size + midCircle.growthrate;
+  midCircle.size = constrain(midCircle.size,0,width,500);
+  fill(midCircle.fill,midCircle.alpha);
+  ellipse(midCircle.x,midCircle.y,midCircle.size);
 
-  sun2.x = sun2.x + sun2.speed;
-  sun2.x = constrain(sun2.x,0,width);
-  sun2.size = map(sun2.x, 0, width, 100, 300);
-  noStroke();
-  fill(255,255,0);
-  ellipse(sun2.x, sun2.y, sun2.size);
+  rect(250,250,mouseX,mouseY);
+
 
 }
