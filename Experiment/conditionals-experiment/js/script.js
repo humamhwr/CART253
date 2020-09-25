43d1d61946b60b1d079d006fa1e5258607f03a9d
Humam Hawara <himoo1999@gmail.com>
2020-09-24 20:12:22 -0400

    i

--- Experiment/conditionals-experiment/js/script.js
+++ Experiment/conditionals-experiment/js/script.js
@@ -1,20 +1,22 @@
-/**************************************************
-Template p5 project
-Pippin Barr
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 1
 }
 
+function setup() {
+  createCanvas(500,500);
+}
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade);
 
+  circle.x = circle.x + circle.speed;
+
+  ellipse(circle.x,circle.y,circle.size);
 }
