5d81c50d47f68ccfb52d71f635c5b23766df803b
Humam Hawara <himoo1999@gmail.com>
2020-09-28 11:51:02 -0400

    added assests and drew the background with gems

--- exercise/exercise2/js/script.js
+++ exercise/exercise2/js/script.js
@@ -1,20 +1,46 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+//plan:
+// green germs as circles on a white background
+// randomly moving sneezing guy
 
-Here is a description of this template p5 project.
-**************************************************/
+let sneeze = {
+  x: 0,
+  y: 250,
+  w: 150,
+  vx: 0,
+  vy: 0,
+  speed: 10,
+};
 
-// setup()
-//
-// Description of setup() goes here.
+let group = {
+  x: 250,
+  y: 250,
+  w: 150,
+  h: 150,
+  image: undefined,
+  tint: {
+    r: 100,
+    g: 100,
+    b: 200,
+},
+};
+
+let numStatic = 500;
+    // Creating the canvas
 function setup() {
-
-}
-
-// draw()
+  createCanvas(windowWidth, windowHeight);
+};
+  // draw()
 //
-// Description of draw() goes here.
+// Creating COVID-19 simulation.
 function draw() {
-
-}
+  //Creating background
+  background(255);
+  //showing germs static
+  for (let i = 0; i < numStatic; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    fill(19, 163, 51);
+    stroke(200, 100, 100);
+    ellipse(x, y, 50);
+  }
+};
