f4cc71280be0bfc046984fe060486f9e682a4526
Humam Hawara <himoo1999@gmail.com>
2020-09-27 23:47:50 -0400

    final push:removed cursor,comments,cleaned

--- Activities/04-dodging-covid/template-p5-project/js/script.js
+++ Activities/04-dodging-covid/template-p5-project/js/script.js
@@ -1,77 +1,77 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+exercise 4 dodging covid19
+ Hummam houara
 
-Here is a description of this template p5 project.
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+
 let covid19 = {
-  x:0,
-  y:250,
+  x: 0,
+  y: 250,
   size: 100,
   vx: 0,
   vy: 0,
   speed: 5,
   fill: {
-    r:255,
-    g:0,
-    b:0
+    r: 255,
+    g: 0,
+    b: 0
   }
 };
 
 let user = {
-  x:250,
-  y:250,
-  size:100,
+  x: 250,
+  y: 250,
+  size: 100,
   fill: 255
 }
-function setup() {
-createCanvas(windowWidth, windowHeight);
 
-covid19.y = random(0,height);
-covid19.vx = covid19.speed;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  covid19.y = random(0, height);
+  covid19.vx = covid19.speed;
+  noCursor();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-background(0);
+  background(0);
 
-// disaplay static
-for (let i =0; i < 1000; i++){
-let x = random(0,width);
-let y = random(0,height);
-stroke(255);
-point(x,y);
-}
-//covid19 movment
-covid19.x = covid19.x + covid19.vx;
-covid19.y = covid19.y + covid19.vy;
+  // disaplay static
+  for (let i = 0; i < 1000; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(255);
+    point(x, y);
+  }
+  //covid19 movment
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
 
-if (covid19.x > width){
-  covid19.x=0;
-  covid19.y = random(0,height);
-}
-// user movment
- user.x = mouseX
- user.y = mouseY
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  }
+  // user movment
+  user.x = mouseX
+  user.y = mouseY
 
-// check for catching covid19
-let d = dist(user.x,user.y,covid19.x,covid19.y);
-if(d < covid19.size/2 + user.size/2){
-  noLoop();
-}
+  // check for catching covid19
+  let d = dist(user.x, user.y, covid19.x, covid19.y);
+  if (d < covid19.size / 2 + user.size / 2) {
+    noLoop();
+  }
 
-// display covid19
-fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-ellipse(covid19.x,covid19.y, covid19.size);
+  // display covid19
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
 
-// display user
-fill(user.fill);
-ellipse(user.x,user.y,user.size);
+  // display user
+  fill(user.fill);
+  ellipse(user.x, user.y, user.size);
 
 }
