db2a37d0fe5aec335b89261f5081df6d8d2dac3a
Humam Hawara <himoo1999@gmail.com>
2020-09-27 23:23:49 -0400

    drew moving circle (covid19) and user circle

---
+++ Activities/04-dodging-covid/template-p5-project/js/script.js
@@ -0,0 +1,63 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+let covid19 = {
+  x:0,
+  y:250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r:255,
+    g:0,
+    b:0
+  }
+};
+
+let user = {
+  x:250,
+  y:250,
+  size:100,
+  fill: 255
+}
+function setup() {
+createCanvas(windowWidth, windowHeight);
+
+covid19.y = random(0,height);
+covid19.vx = covid19.speed;
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+background(0);
+//covid19 movment
+covid19.x = covid19.x + covid19.vx;
+covid19.y = covid19.y + covid19.vy;
+
+if (covid19.x > width){
+  covid19.x=0;
+  covid19.y = random(0,height);
+}
+// user movment
+ user.x = mouseX
+ user.y = mouseY
+
+// display covid19
+fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+ellipse(covid19.x,covid19.y, covid19.size);
+
+// display user
+fill(user.fill);
+ellipse(user.x,user.y,user.size);
+
+}
