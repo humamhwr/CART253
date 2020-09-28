4423554580801d28421d892acd0b35bed9ad3026
Humam Hawara <himoo1999@gmail.com>
2020-09-28 12:14:34 -0400

    E2: removes stroke and fixed the size of the germs, added images and movments

--- exercise/exercise2/js/script.js
+++ exercise/exercise2/js/script.js
@@ -5,42 +5,55 @@
 let sneeze = {
   x: 0,
   y: 250,
-  w: 150,
+  image: undefined,
   vx: 0,
   vy: 0,
-  speed: 10,
+  speed: 10
 };
-
 let group = {
   x: 250,
   y: 250,
-  w: 150,
-  h: 150,
-  image: undefined,
-  tint: {
-    r: 100,
-    g: 100,
-    b: 200,
-},
+  image: undefined
 };
 
-let numStatic = 500;
+// upload images
+function preload() {
+  sneeze.image = loadImage("assets/images/sneeze.png");
+  group.image = loadImage("assets/images/group.png");
+
     // Creating the canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  sneeze.y = random(0, height);
+  sneeze.vx = sneeze.speed;
 };
   // draw()
 //
 // Creating COVID-19 simulation.
 function draw() {
+
   //Creating background
   background(255);
+
   //showing germs static
+  let numStatic = 1000;
   for (let i = 0; i < numStatic; i++) {
     let x = random(0, width);
     let y = random(0, height);
     fill(19, 163, 51);
-    stroke(200, 100, 100);
-    ellipse(x, y, 50);
+    noStroke();
+    circle(x, y, 60);
   }
-};
+
+  // moving the sneezing guy
+  sneeze.x = sneeze.x + sneeze.vx;
+  sneeze.y = sneeze.y + sneeze.vy;
+if (sneeze.x > width) {
+  sneeze.x = 0;
+  sneeze.y = random(0, height);
+}
+// the group moving
+user.x = mouseX
+user.y = mouseY
+}
+}
