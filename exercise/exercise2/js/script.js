876e933baed1eda21c9e7a32a3e3337032f4bea2
Humam Hawara <himoo1999@gmail.com>
2020-09-29 14:36:41 -0400

    E2: trying to show both images and group

--- exercise/exercise2/js/script.js
+++ exercise/exercise2/js/script.js
@@ -1,59 +1,100 @@
 //plan:
 // green germs as circles on a white background
-// randomly moving sneezing guy
+// randomly moving sneezing guy or grouped guy; if the group touched the grouped guy they are good but if they touched the sneezing guy the program stops
 
 let sneeze = {
   x: 0,
   y: 250,
+  w: 150,
+  h: 150,
   image: undefined,
   vx: 0,
   vy: 0,
-  speed: 10
+  speed: 10,
 };
 let group = {
   x: 250,
   y: 250,
+  w: 150,
+  h: 150,
   image: undefined
 };
-
-// upload images
+let masked = {
+  x:250,
+  y:250,
+  w:150,
+  h:150,
+  image:undefined
+}
+let numStatic = 100;
+//Loading images for simulation.
 function preload() {
   sneeze.image = loadImage("assets/images/sneeze.png");
   group.image = loadImage("assets/images/group.png");
-
-    // Creating the canvas
+  masked.image = loadImage("assets/images/masked.png");
+}
+//
+// Creating the canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
   sneeze.y = random(0, height);
   sneeze.vx = sneeze.speed;
-};
-  // draw()
+  masked.y = random(0, height);
+  noCursor();
+}
+// draw()
 //
 // Creating COVID-19 simulation.
 function draw() {
-
   //Creating background
   background(255);
 
-  //showing germs static
-  let numStatic = 1000;
+  //Display static
   for (let i = 0; i < numStatic; i++) {
     let x = random(0, width);
     let y = random(0, height);
-    fill(19, 163, 51);
+    fill(109, 197, 15);
     noStroke();
-    circle(x, y, 60);
+    ellipse(x, y, 50);
+  }
+  //Creating  movement
+  noStroke();
+  sneeze.x += sneeze.vx;
+  sneeze.y += sneeze.vy;
+
+  noStroke();
+  masked.x += masked.vx;
+  masked.y += masked.vy;
+
+  if (sneeze.x > width) {
+    sneeze.x = 0;
+    sneeze.y = random(0, height);
   }
 
-  // moving the sneezing guy
-  sneeze.x = sneeze.x + sneeze.vx;
-  sneeze.y = sneeze.y + sneeze.vy;
-if (sneeze.x > width) {
-  sneeze.x = 0;
-  sneeze.y = random(0, height);
+  if (maksed.x > width) {
+    maksed.x = 0;
+    maksed.y = random(0, height);
+  }
+//  changing the picture
+  if (sneeze.y < height / 2) {
+    image(masked.image, masked.x, masked.y, masked.w, masked.h);;
+  } else {
+    tint(200, 100, 100, 20);
+  }
+  //Display sneeze image
+  image(sneeze.image, sneeze.x, sneeze.y, sneeze.w, sneeze.h);
+  //Display group image
+  image(group.image, group.x, group.y, group.w, group.h);
+  //Display masked image
+  image(masked.image, masked.x, masked.y, masked.w, masked.h);
 }
-// the group moving
-user.x = mouseX
-user.y = mouseY
-}
+//Creating catching effect
+function mouseDragged() {
+  group.x = mouseX;
+  group.y = mouseY;
+  let d = dist(group.x, group.y, sneeze.x, sneeze.y);
+  if (d < sneeze.w / 2 + group.w / 2) {
+    noLoop();
+  }
+
 }
