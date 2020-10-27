e669d2aeb3db41a9205d6d714679f2ccd8e202e2
Humam Hawara <himoo1999@gmail.com>
2020-10-26 21:19:54 -0400

    Activity finished

--- Activities/08-juggling -simulation/js/Paddle.js
+++ Activities/08-juggling -simulation/js/Paddle.js
@@ -1,19 +1,22 @@
 class Paddle {
+
   constructor(w, h) {
     this.width = w;
     this.height = h;
     this.x = 0;
-    this.y = height - this.height / 2;
+    this.y = (height - 10) - this.height/2;
   }
+
   move() {
     this.x = mouseX;
   }
+
   display() {
     push();
-    fill(255);
-    noStroke();
+    fill(266);
     rectMode(CENTER);
     rect(this.x, this.y, this.width, this.height);
     pop();
   }
+
 }
--- Activities/08-juggling -simulation/js/script.js
+++ Activities/08-juggling -simulation/js/script.js
@@ -1,35 +1,61 @@
 "use strict";
-let gravityForce = 0.0025;
 
+/**************************************************
+Juggling Activity
+Melissa Banoen-Garde
+The objective of this activty is to get comfortable with
+defining classes, work with interacting classes via collision,
+and learn to feel okay about acceleration and GRAVITY by
+programming a juggling simulation.
+**************************************************/
+
+// Declaring paddle variable
 let paddle;
 
-let balls = [];
-let numBalls = 3;
+// Declaring gravity variable
+let gravityForce = 0.0025;
 
+// Declaring balls variables with empty array
+let balls = [];
+let numBalls = 30;
+
+// setup()
+//
+// Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  // creating new Paddle object
   paddle = new Paddle(300, 20);
 
+  //for-loop for numBall; allows many balls to appear on screen
   for (let i = 0; i < numBalls; i++) {
-    let x = random(0,width);
-    let y = random (-400, -100);
-    let ball = new Ball(x,y);
-    balls.push(ball);
+    let x = random(0, width);
+    let y = random(-400,-100);
+    // creating new Ball object
+    let ball = new Ball(x, y);
+    balls.push(ball); // pushing ball variable into balls array
   }
 }
 
+// draw()
+//
+// Description of draw() goes here.
 function draw() {
   background(0);
 
+  // calling Paddle.js's methods
   paddle.move();
   paddle.display();
 
-  for (let i = 0; i < balls.length; i++){
-    let ball = balls[i];
-    ball.gravity(gravityForce);
-    ball.move();
-    ball.bounce();
-    ball.display();
+  // for-loop that counts from 0 up to balls.length
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i]; // pulling out ball at current index we're counting from
+     if (ball.active) {  // is it active?
+       ball.gravity(gravityForce);   // calling gravity() with gravityForce as an argument
+       ball.move();
+       ball.bounce(paddle);
+       ball.display();
+     }
   }
 }
