619fdcd5927fa109ba2e70e03f84528b899941eb
Humam Hawara <himoo1999@gmail.com>
2020-10-26 20:04:29 -0400

    trying to find a bug

--- Activities/08-juggling -simulation/js/script.js
+++ Activities/08-juggling -simulation/js/script.js
@@ -1,20 +1,35 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
+let gravityForce = 0.0025;
 
-Here is a description of this template p5 project.
-**************************************************/
+let paddle;
 
-// setup()
-//
-// Description of setup() goes here.
+let balls = [];
+let numBalls = 3;
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0,width);
+    let y = random (-400, -100);
+    let ball = new Ball(x,y);
+    balls.push(ball);
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++){
+    let ball = balls[i];
+    ball.gravity(gravityForce);
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
 }
