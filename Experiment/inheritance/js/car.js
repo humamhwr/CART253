b645bac298af1dd20a7ef3b3d61f5be04b4bde42
Humam Hawara <himoo1999@gmail.com>
2020-11-10 00:14:08 -0500

    comments

--- Experiment/inheritance/js/car.js
+++ Experiment/inheritance/js/car.js
@@ -1,19 +1,16 @@
 class Car extends Vehicle {
+  // Create a new Car object that moves to the right
   constructor(x, y) {
     super(x, y);
-
     this.width = 50;
     this.height = 20;
     this.vx = 5;
   }
 
+  // Display the car
   display() {
     push();
-    // NEW! First we set the red fill
     fill(255, 0, 0);
-    // NEW! Then we call the superclass's version of display(), which will display
-    // a rectangle with the Car's dimensions at the Car's position. Because we set
-    // the fill to red before this, the rectangle will be red.
     super.display();
     pop();
   }
