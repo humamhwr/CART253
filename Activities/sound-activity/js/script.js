000028eac238e44efec10ee1cdf4c47809e96b59
Humam Hawara <himoo1999@gmail.com>
2020-11-11 08:50:10 -0500

    E6: Started exercise 6

--- Activities/sound-activity/js/script.js
+++ Activities/sound-activity/js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 /**************************************************
 Template p5 project
 Pippin Barr
@@ -8,13 +9,34 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
 
+let balls = [];
+
+
+function setup() {
+  createCanvas(600, 600);
+  userStartAudio();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed(){
+  createBall(mouseX,mouseY);
+}
+
+function createBall(x, y) {
+  let ball = new Ball(x,y);
+  balls.push(ball);
 }
