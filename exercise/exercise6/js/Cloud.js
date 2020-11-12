0f214bd99eb398516339043dfa3f888a351660eb
Humam Hawara <himoo1999@gmail.com>
2020-11-12 02:31:21 -0500

    E6: everything working before adding the states and the losing aspect

--- exercise/exercise6/js/Cloud.js
+++ exercise/exercise6/js/Cloud.js
@@ -1,27 +1,29 @@
+//moving clouds for a nice sky effect
 class Cloud {
-  
   constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.width = 50;
-      this.height = 20;
-      this.vx = 10;
-    }
+    this.x = x;
+    this.y = y;
+    this.width = 3000;
+    this.height = 500;
+    this.vx = 3;
+    this.vy = 0;
+  }
 
-    // Move the cloud according to its velocity
-    move() {
-      this.x += this.vx;
-      this.y += this.vy;
-    }
+  // Moving the cloud using the velocity
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
 
-    // Wrap the cloud if it reaches the right edge
-    wrap() {
-      if (this.x > width) {
-        this.x -= width;
-      }
-    }
-
-    // Display the cloud as a rectangle
-    image(cloudImage)
+  // Wrap the cloud when it reaches the edge
+  wrap() {
+    if (this.x > width) {
+      this.x -= width;
     }
   }
+
+  // Display the cloud with it's features
+  display() {
+    image(cloudImage, this.vx, this.vy, this.width, this.height, this.x, this.y);
+  }
+}
--- exercise/exercise6/js/script.js
+++ exercise/exercise6/js/script.js
@@ -1,3 +1,9 @@
+//Humam Houara
+// this simulation is to experemint with audio, the user shoud keep making noise for the bird to fly.
+// once the bird drops, the playes loses.
+//*****************************************
+
+
 let mic;
 let birdImage = {
   x: undefined,
@@ -5,26 +11,23 @@ let birdImage = {
   width: 2,
   height: 2,
 };
-
-
-
 let clouds = [];
-let numclouds = 5;
+let numclouds = 10;
 
-
+//preloading media
 function preload() {
   birdImage = loadImage('assets/images/bird.png')
   cloudImage = loadImage('assets/images/cloud.png')
 };
 
-
 function setup() {
+  //creating canvas and accessing audio input
   let cnv = createCanvas(600, 600);
   cnv.mousePressed(userStartAudio);
 
   mic = new p5.AudioIn();
   mic.start();
-
+  //showing the cloud
   for (let i = 0; i < numclouds; i++) {
     let x = random(0, width);
     let y = random(0, height);
@@ -42,8 +45,7 @@ function draw() {
     cloud.wrap();
     cloud.display();
   }
-
-
+  //making the effect of the bird flying from the mic input
   micLevel = mic.getLevel();
   let y = height - micLevel * height;
   image(birdImage, width / 2, y);
